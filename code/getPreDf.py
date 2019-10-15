da77f3df6d6ed2244c4cc225a851b69a062c3d73
farc <farc@outlook.es>
2019-10-15 12:34:19 +0200

    update

--- code/getPreDf.py
+++ code/getPreDf.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn.metrics import r2_score
 
 def errorAbsoluto(row):
     error = row.sf-row.pred
@@ -12,4 +13,6 @@ def getDf(y_test,y_pred):
     df['errorAbs']=df.apply(lambda row:errorAbsoluto(row),axis=1)
     df=df.reset_index().drop(columns='index')
     df=df.reset_index()
+    print('Valores predecidos correspondientes al 40% de los datos:',df.shape[0],
+      'r2_score:',r2_score(df.sf, df.pred))
     return df
