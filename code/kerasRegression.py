8c87eda10b5a75afc528e192ea0d4781c6860dbb
farc <farc@outlook.es>
2019-10-16 14:54:46 +0200

    update

--- code/kerasRegression.py
+++ code/kerasRegression.py
@@ -3,6 +3,17 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 from sklearn.metrics import r2_score
+from sklearn.preprocessing import MinMaxScaler
+
+def getScaler(data):
+    scaler = MinMaxScaler()
+    scaler.fit(data.drop(columns='sf'))
+    return scaler
+
+def fitNgetScaler(data):
+    scaler = MinMaxScaler()
+    scaler.fit(data.drop(columns='sf'))
+    return scaler
 
 class PrintDot(keras.callbacks.Callback):
     def on_epoch_end(self, epoch, logs):
@@ -11,7 +22,7 @@ class PrintDot(keras.callbacks.Callback):
 
 def getData():
     data= pd.read_csv('./input/especNum.csv').drop(columns='Unnamed: 0')
-    train_dataset = data.sample(frac=0.2,random_state=None)
+    train_dataset = data.sample(frac=0.8,random_state=None)
     test_dataset = data.drop(train_dataset.index)
     train_labels = train_dataset.pop('sf')
     test_labels = test_dataset.pop('sf')
@@ -19,15 +30,15 @@ def getData():
 
 def build_model(train_dataset):
     model = keras.Sequential([
-    layers.Dense(32, activation='relu', input_shape=[len(train_dataset.keys())]),
-    layers.Dense(32, activation='relu'),
+    layers.Dense(64, activation='relu', input_shape=[len(train_dataset.keys())]),
+    layers.Dense(64, activation='relu'),
     layers.Dense(1,activation='linear')])
 
-    #optimizer = tf.keras.optimizers.RMSprop(0.001)
+    optimizer = tf.keras.optimizers.RMSprop(0.001)
     #optimizer = tf.keras.optimizers.SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
     #optimizer = tf.keras.optimizers.Adagrad(learning_rate=0.01)
     #optimizer= tf.keras.optimizers.Adadelta(learning_rate=1.0, rho=0.95)
-    optimizer=tf.keras.optimizers.Adam(learning_rate=0.001, beta_1=0.9, beta_2=0.999, amsgrad=False)
+    #optimizer=tf.keras.optimizers.Adam(learning_rate=0.001, beta_1=0.9, beta_2=0.999, amsgrad=False)
     #optimizer=tf.keras.optimizers.Adamax(learning_rate=0.002, beta_1=0.9, beta_2=0.999)perce no valer
     #optimizer=tf.keras.optimizers.Nadam(learning_rate=0.002, beta_1=0.9, beta_2=0.999)
     
@@ -40,11 +51,15 @@ def build_model(train_dataset):
 def eternal():
     early_stop = keras.callbacks.EarlyStopping(monitor='val_loss', patience=10)
     EPOCHS = 1000
-    score=0.9912
-    count=6
+    score=0.9916
+    count=3
+    scaler=getScaler(pd.read_csv('./input/especNum.csv').drop(columns='Unnamed: 0'))
     while(True):
         train_dataset, test_dataset, train_labels ,test_labels=getData()
+        
         model = build_model(train_dataset)
+        train_dataset=scaler.transform(train_dataset)
+        test_dataset=scaler.transform(test_dataset)
         model.fit(train_dataset, train_labels, epochs=EPOCHS,
                     validation_split = 0.2, verbose=0, callbacks=[early_stop, PrintDot()])
         y_pred= model.predict(test_dataset).flatten()
@@ -53,7 +68,7 @@ def eternal():
         if newScore>score:
             print('YEEEHHHHHHHHHHHHHHHHHHHHH')
             score=newScore
-            model.save('../output/keras/kerasAdam{}.h5'.format(count))
+            model.save('./output/keras/kerasRMSScaler{}.h5'.format(count))
             count+=1
         del model
 
